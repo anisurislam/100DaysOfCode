44a8ca5ec731e3acfc9cf3e5a2283fcd9856fa12
Anisur Islam <aianis012355@gmail.com>
2024-09-13 11:54:48 +0600

    Day 7 added to 100DaysOfCode

---
+++ Day7_(Stopwatch)/main.js
@@ -0,0 +1,85 @@
+let millisecondsDisplay = document.querySelector(".millisecond h2");
+let secondsDisplay = document.querySelector(".second h2");
+let minutesDisplay = document.querySelector(".minute h2");
+let hoursDisplay = document.querySelector(".hour h2");
+
+///////////////////////////
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+///////////////////////////
+
+function stopwatch() {
+    milliseconds += 1;
+    if (milliseconds <= 9) {
+        millisecondsDisplay.innerHTML = "0" + milliseconds;
+    }
+    else {
+        millisecondsDisplay.innerHTML = milliseconds;
+    }
+    if (milliseconds >= 99) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds <= 9) {
+            secondsDisplay.innerHTML = "0" + seconds;
+        } else {
+            secondsDisplay.innerHTML = seconds;
+        }
+
+    }
+    if (seconds >= 59) {
+        seconds = 0;
+        minutes++;
+        if (minutes <= 9) {
+            minutesDisplay.innerHTML = "0" + minutes;
+        } else {
+            minutesDisplay.innerHTML = minutes;
+        }
+    }
+
+    if (minutes >= 59) {
+        minutes = 0;
+        hours++;
+        if (hours <= 9) {
+            hoursDisplay.innerHTML = "0" + hours;
+        } else {
+            hoursDisplay.innerHTML = hours;
+        }
+    }
+}
+
+
+
+
+
+let stopwatchInterval;
+let playPause = document.querySelector(".play-pause");
+playPause.addEventListener("click", () => {
+
+    if (playPause.innerHTML == '<i class="fa-solid fa-pause"></i>') {
+        playPause.innerHTML = '<i class="fa-solid fa-play"></i>';
+        clearInterval(stopwatchInterval);
+        playPause.title = "Play";
+
+    } else {
+        playPause.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        playPause.title = "Pause";
+        stopwatchInterval = setInterval(stopwatch, 10);
+    }
+
+})
+
+let reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+    clearInterval(stopwatchInterval);
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    millisecondsDisplay.innerHTML = "00";
+    secondsDisplay.innerHTML = "00";
+    minutesDisplay.innerHTML = "00";
+    hoursDisplay.innerHTML = "00";
+    playPause.innerHTML = '<i class="fa-solid fa-play"></i>';
+})
